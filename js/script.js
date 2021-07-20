88a611a45cd9b3ea9ca1f8d3f3cc09db8b352ec8
Chhavi Arora <chhaviarora111@gmail.com>
2021-07-21 04:15:45 +0530

    for desktop

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
 //Overlay fade out on load
-$(window).load(function (i) {
-    $('#loading').delay(10).fadeOut('slow');
-});
+// $(window).load(function (i) {
+//     $('#loading').delay(10).fadeOut('slow');
+// });
 
 // $(window).load(function () {
 //     $('#loading').hide();
 // });
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0px";
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("navbar").style.top = "0px";
+//     } else {
+//         document.getElementById("navbar").style.top = "-90px";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
     } else {
-        document.getElementById("navbar").style.top = "-90px";
+        mybutton.style.display = "none";
     }
-    prevScrollpos = currentScrollPos;
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 
