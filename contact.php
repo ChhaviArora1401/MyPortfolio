587367815f357827c622c1872175094471e41913
Chhavi Arora <chhaviarora111@gmail.com>
2021-07-31 23:50:56 +0530

    >

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+    $name = $POST['name'];
+    $subject = $_POST['name'];
+    $name = $_POST['subject'];
+    $mailForm = $_POST['mail'];
+    $message = $_POSt['message'];
+
+    $mailTo = "chhaviarora1401@gmail.com";
+    $headers = "From: ".$mailForm;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
