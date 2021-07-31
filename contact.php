cbd254eab9b3fefa6ac87dfc3dd8e7f911990656
Chhavi Arora <chhaviarora111@gmail.com>
2021-07-31 23:53:45 +0530

    >

--- contact.php
+++ contact.php
@@ -12,5 +12,5 @@ if (isset($_POST['submit'])) {
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html?mailsend");
 }
