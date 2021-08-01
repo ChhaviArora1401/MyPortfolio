037468ccd1a8d24997760148932f97b89f32b8da
Chhavi Arora <chhaviarora111@gmail.com>
2021-08-02 01:39:48 +0530

    .

--- contact.php
+++ contact.php
@@ -3,8 +3,11 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
+
 $to = "chhaviarora1401@gmail.com";
+
 $subject = "Mail From website";
+
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
